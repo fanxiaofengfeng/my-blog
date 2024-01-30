85c27c59915f030bbcef2f5c5d56a898b8ce512a
fanxiaofeng <fanxiaofeng@qianlima.com>
2024-01-30 14:57:21 +0800

    搜索3

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -82,6 +82,7 @@ export default hopeTheme({
   plugins: {
     //开启博客功能
     blog: true,
+    search: true,
 
     // install @waline/client before enabling it
     // WARNING: This is a test server for demo only.
@@ -156,21 +157,21 @@ export default hopeTheme({
       // install @vue/repl before enabling it
       // vuePlayground: true,
     },
-    searchPro: {
-    //索引全部内容
-      indexContent: true,
-     // 为分类和标签添加索引
-     customField: [
-       {
-         getter: (page) => page.frontmatter.category,
-         formatter: "分类: $content",
-       },
-       {
-         getter: (page) => page.frontmatter.tag,
-         formatter: "标签: $content",
-       }
-     ]
-    }
+    // searchPro: {
+    // //索引全部内容
+    //   indexContent: true,
+    //  // 为分类和标签添加索引
+    //  customField: [
+    //    {
+    //      getter: (page) => page.frontmatter.category,
+    //      formatter: "分类: $content",
+    //    },
+    //    {
+    //      getter: (page) => page.frontmatter.tag,
+    //      formatter: "标签: $content",
+    //    }
+    //  ]
+    // }
     // uncomment these if you want a PWA
     // pwa: {
     //   favicon: "/favicon.ico",
