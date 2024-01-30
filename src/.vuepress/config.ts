d1440d704415480526915765d589f9eeba3ad7b3
fanxiaofeng <fanxiaofeng@qianlima.com>
2024-01-30 11:57:40 +0800

    主题强制深色

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import {hopeTheme} from "vuepress-theme-hope";
 
 export default defineUserConfig({
   base: "/",
@@ -9,8 +10,9 @@ export default defineUserConfig({
   title: "梵梵爱学习",
   description: "vuepress-theme-hope 的博客演示",
 
-  theme,
-
+  theme: hopeTheme({
+    darkmode: "enable"
+  }),
   // Enable it with pwa
   // shouldPrefetch: false,
 });
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -4,6 +4,7 @@ import sidebar from "./sidebar.js";
 import { MR_HOPE_AVATAR } from "./logo.js";
 
 export default hopeTheme({
+  locales: undefined,
   hostname: "https://mister-hope.github.io",
 
   author: {
