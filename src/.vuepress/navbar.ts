abc3efc3f373cb49aab6a16fafa6b07fb9b5b283
fanxiaofeng <fanxiaofeng@qianlima.com>
2024-01-30 15:31:44 +0800

    搜索7

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -10,7 +10,7 @@ export default navbar([
       {
         text: "苹果",
         icon: "pen-to-square",
-        prefix: "apple/",
+        prefix: "spring/",
         children: [
           { text: "苹果1", icon: "pen-to-square", link: "1" },
           { text: "苹果2", icon: "pen-to-square", link: "2" },
