6a078e4f89806023d4a6919d84c29d026b110abd
fanxiaofengfeng <1821184005@qq.com>
2024-02-07 21:52:20 +0800

    修改导航

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -14,45 +14,46 @@ export default navbar([
     link: "/posts/mysql",
   },
   {
-    text: "博文",
+    text: "MongoDB",
     icon: "pen-to-square",
-    prefix: "/posts/",
-    children: [
-      {
-        text: "spring",
-        icon: "pen-to-square",
-        prefix: "spring/",
-        children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-          "3",
-          "4",
-        ],
-      },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "资源/",
-        children: [
-          {
-            text: "香蕉 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "香蕉 2",
-            icon: "pen-to-square",
-            link: "2",
-          },
-          "3",
-          "4",
-        ],
-      },
-      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-      { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
-    ],
+    link: "/post/mongodb"
+    // prefix: "/posts/",
+    // children: [
+    //   {
+    //     text: "spring",
+    //     icon: "pen-to-square",
+    //     prefix: "spring/",
+    //     children: [
+    //       { text: "苹果1", icon: "pen-to-square", link: "1" },
+    //       { text: "苹果2", icon: "pen-to-square", link: "2" },
+    //       "3",
+    //       "4",
+    //     ],
+    //   },
+    //   {
+    //     text: "香蕉",
+    //     icon: "pen-to-square",
+    //     prefix: "资源/",
+    //     children: [
+    //       {
+    //         text: "香蕉 1",
+    //         icon: "pen-to-square",
+    //         link: "1",
+    //       },
+    //       {
+    //         text: "香蕉 2",
+    //         icon: "pen-to-square",
+    //         link: "2",
+    //       },
+    //       "3",
+    //       "4",
+    //     ],
+    //   },
+    //   { text: "樱桃", icon: "pen-to-square", link: "cherry" },
+    //   { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
+    //   "tomato",
+    //   "strawberry",
+    // ],
   },
   {
     text: "spring系列",
@@ -60,10 +61,11 @@ export default navbar([
     prefix: "/posts/spring",
     children: [
     ],
+
   },
-  {
-    text: "文档",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
-  },
+  // {
+  //   text: "文档",
+  //   icon: "book",
+  //   link: "https://theme-hope.vuejs.press/zh/",
+  // },
 ]);
